6e8f10405a6b7636ba83fb2f40dee0b93ccff11f
diegoluanmartins <diegoluanmartins@gmail.com>
2024-02-25 19:51:51 -0300

    Adicionando variável de ambiente

--- lambda_function.py
+++ lambda_function.py
@@ -1,10 +1,11 @@
 import json
 import log
-
+import os
+  
 def lambda_handler(event, context):
     log.log("Event: " + event)
     return {
         'statusCode': 200,
-        'hello': 'world',
+        'ENV_VAR_1': os.environ['ENV_VAR_1'],
         'body': json.dumps(event)
     }
